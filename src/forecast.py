028f505e5a1d3729eaadea0fe9251cf6931aaa71
big-d0g <becca.starr@kayenta.io>
2024-02-26 15:12:23 +0000

    Add constants to remove magic numbers

--- src/forecast.py
+++ src/forecast.py
@@ -2,11 +2,18 @@ from datetime import datetime
 from collections import defaultdict
 
 
+MORNING_STARTING_HOUR = 6
+AFTERNOON_STARTING_HOUR = 12
+NIGHT_STARTING_HOUR = 18
+
+ROUNDING_PRECISION = 2
+
 class Entry:
     def __init__(self, average_temperature, probability_of_rain, date_time):
         self.average_temperature = average_temperature
         self.probability_of_rain = probability_of_rain
         self.date_time = date_time
+
 
 
 def group_entries_by_day(data, grp_day):
@@ -28,11 +35,11 @@ def process_each_day(entries) -> dict:
     for e in entries:
         entry_time = datetime.fromisoformat(e.date_time.replace('Z', '+00:00'))
         # collect morning period entries
-        if 6 <= entry_time.hour < 12:
+        if MORNING_STARTING_HOUR <= entry_time.hour < AFTERNOON_STARTING_HOUR:
             morning_t.append(e.average_temperature)
             morning_r.append(e.probability_of_rain)
         # collection afternoon period entries
-        elif 12 <= entry_time.hour < 18:
+        elif AFTERNOON_STARTING_HOUR <= entry_time.hour < NIGHT_STARTING_HOUR:
             afternoon_t.append(e.average_temperature)
             afternoon_r.append(e.probability_of_rain)
 
@@ -41,12 +48,12 @@ def process_each_day(entries) -> dict:
         "morning_average_temperature": "Insufficient forecast data" if not morning_t else round(
             sum(morning_t) / len(morning_t)),
         "morning_chance_of_rain": "Insufficient forecast data" if not morning_r else round(
-            sum(morning_r) / len(morning_r), 2),
+            sum(morning_r) / len(morning_r), ROUNDING_PRECISION),
         # if no afternoon data, report insufficient data
         "afternoon_average_temperature": "Insufficient forecast data" if not afternoon_t else round(
             sum(afternoon_t) / len(afternoon_t)),
         "afternoon_chance_of_rain": "Insufficient forecast data" if not afternoon_r else round(
-            sum(afternoon_r) / len(afternoon_r), 2),
+            sum(afternoon_r) / len(afternoon_r), ROUNDING_PRECISION),
         "high_temperature": max(all_t),
         "low_temperature": min(all_t)
     }
