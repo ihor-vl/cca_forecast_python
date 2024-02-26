ccb8c476a001d3c97103a8cd5b5cd658d8b77144
Scandie <grizly.vl@gmail.com>
2024-02-26 17:19:44 +0200

    Move period calculation to Entry class

--- src/forecast.py
+++ src/forecast.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 from collections import defaultdict
 
+MORNING = "morning"
+AFTERNOON = "afternoon"
+
 
 MORNING_STARTING_HOUR = 6
 AFTERNOON_STARTING_HOUR = 12
@@ -8,12 +11,19 @@ NIGHT_STARTING_HOUR = 18
 
 ROUNDING_PRECISION = 2
 
+
 class Entry:
     def __init__(self, average_temperature, probability_of_rain, date_time):
         self.average_temperature = average_temperature
         self.probability_of_rain = probability_of_rain
         self.date_time = date_time
 
+    def get_period(self):
+        entry_time = datetime.fromisoformat(self.date_time.replace('Z', '+00:00'))
+        if MORNING_STARTING_HOUR <= entry_time.hour < AFTERNOON_STARTING_HOUR:
+            return MORNING
+        elif AFTERNOON_STARTING_HOUR <= entry_time.hour < NIGHT_STARTING_HOUR:
+            return AFTERNOON
 
 
 def group_entries_by_day(data, grp_day):
@@ -32,16 +42,13 @@ def process_each_day(entries) -> dict:
     morning_t, morning_r, afternoon_t, afternoon_r = [], [], [], []
     all_t = [entry.average_temperature for entry in entries]
 
-    for e in entries:
-        entry_time = datetime.fromisoformat(e.date_time.replace('Z', '+00:00'))
-        # collect morning period entries
-        if MORNING_STARTING_HOUR <= entry_time.hour < AFTERNOON_STARTING_HOUR:
-            morning_t.append(e.average_temperature)
-            morning_r.append(e.probability_of_rain)
-        # collection afternoon period entries
-        elif AFTERNOON_STARTING_HOUR <= entry_time.hour < NIGHT_STARTING_HOUR:
-            afternoon_t.append(e.average_temperature)
-            afternoon_r.append(e.probability_of_rain)
+    for entry in entries:
+        if entry.get_period() == MORNING:
+            morning_t.append(entry.average_temperature)
+            morning_r.append(entry.probability_of_rain)
+        elif entry.get_period() == AFTERNOON:
+            afternoon_t.append(entry.average_temperature)
+            afternoon_r.append(entry.probability_of_rain)
 
     summary = {
         # if no morning data, report insufficient data
